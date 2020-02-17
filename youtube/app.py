2c66ae380f0e96960145fa4d83bd0d62a220a618
yassataiseer <56459253+yassataiseer@users.noreply.github.com>
2020-02-17 11:01:48 -0500

    Update app.py

--- youtube/app.py
+++ youtube/app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template,request,session
 import json
 import urllib.request
 
+import sqlite3 
+  
+conn = sqlite3.connect('database.db', check_same_thread=False) 
+c = conn.cursor() 
+
 key = "AIzaSyDIc87kbZUwAjuqyyKtvwCHytHAS6n_FU4"
 
 
@@ -10,18 +15,28 @@ app = Flask(__name__)
 def index():
     return render_template("name.html")
 
-
-
 @app.route('/', methods=['POST'])
 def getvalue():
     youtuber= request.form['text']
-    data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername="+youtuber+"&key="+key).read()
+    a = youtuber.replace(" ","")
+    data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername="+a+"&key="+key).read()
     subs = json.loads(data)["items"][0]["statistics"]["subscriberCount"]
     print(youtuber)
     print(data)
     print(subs)
-    return render_template("count.html",youtuber=youtuber,subs=subs)  
+    c.execute("INSERT INTO RecordONE (Number, Name) VALUES(?, ?)", (youtuber, subs)) 
+    conn.commit() 
+    return render_template("count.html",youtuber=youtuber,subs=subs) 
+    exit()
+
+    getvalue() 
+    c.close() 
+    conn.close() 
+
+    
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
